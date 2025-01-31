ae489353d7cdbcd4e2c23b79aa35641f69bd7cb6
Rohit <rohit.henceforth@gmail.com>
2025-01-31 14:29:27 +0530

    change endpoint for main

--- chatbot.js
+++ chatbot.js
@@ -804,7 +804,7 @@
     constructor() {
       this.secret_key = localStorage.getItem("secret_key");
       this.agent_id = localStorage.getItem("ai_agent");
-      this.apiEndpoint = "https://dev.qixs.ai:3003/";
+      this.apiEndpoint = "https://qixs.ai:3001/";
       this.initialized = false;
       this.messageHistory = [];
       this.chatId = null;
